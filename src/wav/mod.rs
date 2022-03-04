930d54d2f1e09b433dc3607c17d1e1aca6119f29
kurotakazuki <kabisakanakabi@gmail.com>
2022-03-05 03:37:52 +0900

    Public Chunk

--- src/wav/mod.rs
+++ src/wav/mod.rs
@@ -1,7 +1,7 @@
 pub use self::io::{
     WavFrameReader, WavFrameReaderKind, WavFrameWriter, WavFrameWriterKind, WavReader, WavWriter,
 };
-pub use self::metadata::WavMetadata;
+pub use self::metadata::{WavMetadata, Chunk};
 
 mod io;
 mod metadata;
